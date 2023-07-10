e96df01bb27ded5a13df00b718a60af08d907a77
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2023-07-10 14:34:02 -0300

    alteração meia boca

---
+++ script.js
@@ -0,0 +1,29 @@
+const tcpy = document.getElementById("copiar")
+const texto = document.getElementById("email")
+
+function Linkedin(){
+    var display = document.getElementById("loading").style.display;
+if(display == "none")
+    document.getElementById("loading").style.display = 'block',
+    setTimeout( function() { window.location = 'https://www.linkedin.com/in/tiago-leonardo-schroeder-44200a241/' }, 750 );
+else
+    document.getElementById("loading").style.display = 'none';
+    document.getElementById("paragrafo").style.display = 'none';
+}
+
+function Github(){
+    var display = document.getElementById("loading").style.display;
+if(display == "none")
+    document.getElementById("loading").style.display = 'block',
+    setTimeout( function() { window.location = 'https://github.com/TiagoLeoSchroeder' }, 750 );
+else
+    document.getElementById("loading").style.display = 'none';
+    document.getElementById("paragrafo").style.display = 'none';
+}
+
+
+tcpy.addEventListener("click",(evt)=>{
+    texto.select()
+    texto.setSelectionRange(0, 9999)
+    navigator.clipboard.writeText(texto.value)
+})
